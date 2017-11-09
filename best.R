fba049573094f377b878975114ca5a4ade9ec907
connermcb <connermcb@yahoo.com>
2017-11-09 11:48:45 -0600

    skeleton of  function

---
+++ best.R
@@ -0,0 +1,11 @@
+## Function for Week 3 - R Programming Assignment
+## Uses data from Hospital Compare web site (http://hospitalcompare.hhs.gov)
+## run by the U.S. Department of Health and Human Services.
+
+
+best <- function(state, outcome) {
+  ## Read outcome data
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+}
