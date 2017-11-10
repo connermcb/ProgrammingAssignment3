71876b63b791bb32d6d73c3cd5592c82aa0b50b8
connermcb <connermcb@yahoo.com>
2017-11-10 09:38:48 -0600

    add data files, 1st working implementation

--- rankhospital.R
+++ rankhospital.R
@@ -4,10 +4,14 @@
 ## `outcome-of-care-measures.csv` file and returns a character vector with the name
 ## of the hospital that has the ranking specified by the num argument.
 
-rankhospital <- function(state, outcome, num = "best") {
+library(dplyr)
+library(rlang)
+library(stringr)
+
+rankhospital <- function(state, outcome, num = "best", f="outcome-of-care-measures.csv") {
   ## Format args
   state <- toupper(state)
-  outcome <- tolower(outcome)
+  outcome <- str_replace_all(tolower(outcome), pattern = " ", repl="")
   
   ## Read and subset outcome data
   if(!file.exists(f)){
@@ -15,11 +19,17 @@ rankhospital <- function(state, outcome, num = "best") {
   }
   outcome_data <- read.csv(f)
   outcome_data <- subset(outcome_data, State==state)
-  
+
   ## Rename columns of interest
-  outcome_names <- c("heart attack", "heart failure", "pneumonia")
+  outcome_names <- c("heartattack", "heartfailure", "pneumonia")
   colnames(outcome_data)[c(11, 17, 23)] <- outcome_names 
-  
+
+  ## Change class of variable
+  outcome_data[, outcome] <- as.numeric(
+                              as.matrix(
+                                outcome_data[, outcome]
+                              )
+  )
   ## Check that `state` is valid
   state_names <- unique(outcome_data$State)
   if(!(state %in% state_names)){
@@ -33,4 +43,21 @@ rankhospital <- function(state, outcome, num = "best") {
   
   ## Return hospital name in that state with the given rank
   ## 30-day death rate
+  outcome_col <- parse_quosure(outcome)
+  arrgnd_data <- outcome_data%>%
+    select(outcome, Hospital.Name)%>%
+    arrange(!!outcome_col, Hospital.Name)
+  if(num=="best"){
+    return(arrgnd_data[1])
+  }
+  if(num=="worst"){
+    arrgnd_data <- arrgnd_data%>%
+      na.omit()
+    return(arrgnd_data[nrow(arrgnd_data),])
+  }
+  return(arrgnd_data$Hospital.Name[num])
+  
 }
+
+rankhospital("TX", "heart failure", 4)
+rankhospital("MD", "heart attack", "worst")
